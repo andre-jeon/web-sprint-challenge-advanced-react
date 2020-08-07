2ad4761c3d915e1e138f0ddfe1d0fabcfc8ed067
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-08-07 13:42:30 -0400

    testing testing testing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -14,5 +14,8 @@ test("form header renders", () => {
     expect(header).toBeInTheDocument();
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+    
+});
 
