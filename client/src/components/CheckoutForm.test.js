c1a817dbc088e935a593a4ddbf25479264dbf6d4
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-08-07 13:29:42 -0400

    working on the testing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,6 +4,15 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    // render a CheckoutForm component
+    render(<CheckoutForm />);
+    // 'query' the virtual DOM for the header element
+    const header = screen.getByText(/checkout form/i);
+    // assert that the header is displayed
+    expect(header).toBeInTheDOM();
+    expect(header).toBeInTheDocument();
+});
 
 test("form shows success message on submit with form details", () => {});
+
